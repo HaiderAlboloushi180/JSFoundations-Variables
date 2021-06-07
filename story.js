121d3acfc8405f37deeaa4af02162bce6ceb9986
HaiderAlboloushi180 <halbolou05@gmail.com>
2021-06-08 02:31:21 +0300

    Done

---
+++ story.js
@@ -0,0 +1,5 @@
+let person = 'Haider'
+let age = 16
+let hobby = "coding"
+
+console.log(`Mr. ${person}, in 33 years you will be ${age+33} years old. By then you'll have decades of ${hobby} practice`);
